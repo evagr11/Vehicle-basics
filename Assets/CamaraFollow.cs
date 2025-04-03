e7ca1d4e3291cd550f4832aa0a6e39055731a04e
evagr11 <evagallardor@icloud.com>
2025-04-03 20:02:18 +0200

    Cámara
    Día 2 terminado, código de cámara hecho (posible revisión y cambio de parámetros)

--- Assets/CamaraFollow.cs
+++ Assets/CamaraFollow.cs
@@ -1,34 +1,36 @@
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 public class CamaraFollow : MonoBehaviour
 {
     [SerializeField] Transform player;
     [SerializeField] float followSmooth = 0.2f;
-    [SerializeField] float rotationSmooth = 1;
     [SerializeField] float lookAheadDistance = 30; // mirar hacia adelante distancia
-    [SerializeField] float lookAheadSmooth = .2f; // mirar hacia adelante suave
     [SerializeField] float minDistanceToTarget = 5; // Distancia mínima al objetivo
     [SerializeField] float verticalOffset = 2;
-
     Vector3 currentVelocity;
 
+    /*
+    [SerializeField] float rotationSmooth = 1;
+    [SerializeField] float lookAheadSmooth = .2f; // mirar hacia adelante suave
+    */
     private void FixedUpdate()
     {
-        transform.LookAt(player.transform);
+        transform.LookAt(player.transform.position + lookAheadDistance * player.transform.forward);
 
         Vector3 targetPosition = player.position + minDistanceToTarget * -player.transform.forward + verticalOffset * Vector3.up;
 
-        float vectorDistance = (player.position.magnitude - transform.position.magnitude);
-
         transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref currentVelocity, followSmooth);
+
+        float vectorDistance = (player.position.magnitude - transform.position.magnitude);
 
         if (vectorDistance < minDistanceToTarget)
         {
-            Vector3 directionAwayFromPlayer = (player.position - transform.position).normalized;
+            Vector3 directionAwayFromPlayer = (player.position - transform.position);
 
-            transform.position = Vector3.SmoothDamp(transform.position, directionAwayFromPlayer, ref currentVelocity, followSmooth);
+            Vector3 newPosition = minDistanceToTarget / directionAwayFromPlayer.magnitude * directionAwayFromPlayer;
 
-
+            transform.position = player.position - directionAwayFromPlayer.normalized * minDistanceToTarget;
 
         }
 
